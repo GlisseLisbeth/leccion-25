8d60b65f4be927661904bea563880f69b6aac9a4
GlisseLisbeth <glisselisbeth@gmail.com>
2017-04-24 13:07:29 -0500

    add files

---
+++ assets/js/app.js
@@ -0,0 +1,33 @@
+function creandoDiv(getURL){
+  var contenedor = document.getElementById('contenedor');
+  var div = document.createElement('div');
+  var img = document.createElement('img');
+  img.setAttribute('src',getURL);
+  div.classList.add('contenedor-imagen');
+  div.appendChild(img);
+  contenedor.appendChild(div);
+}
+
+
+var inputURL = document.getElementById('url');
+function getURL(e){
+  if(e.keyCode ==13){
+    if(!inputURL.value.length==0){
+      if (/.(gif|jpeg|jpg|png)$/i.test(inputURL.value) == false)
+      {
+        alert('Ingrese correctamente la url con extension .gif, .jpeg, .jpg y .png');
+        inputURL.focus();
+        return false;
+      }
+      else{
+        creandoDiv(this.value);
+        this.value =  "";
+      }
+    }
+    else{
+      alert('El campo esta vacio');
+    }
+  }
+}
+
+inputURL.onkeydown = getURL;
